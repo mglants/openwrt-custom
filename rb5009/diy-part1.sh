18f847316c7b3d364a5f47e14c0aa4f7dce09545
Mark Glants <mark@glants.xyz>
2025-01-08 19:10:25 +0300

    fix: rb 5009

--- rb5009/diy-part1.sh
+++ rb5009/diy-part1.sh
@@ -16,8 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# releasemihomo=$(curl -s -L https://github.com/MetaCubeX/mihomo/releases/latest | grep "title>Release" | cut -d " " -f 4)
-# curl -L https://github.com/MetaCubeX/mihomo/releases/download/$releasemihomo/mihomo-linux-arm64-$releasemihomo.gz -o /tmp/clash.gz
-# mkdir -p files/opt/clash/bin && \
-# gunzip -c /tmp/clash.gz > files/opt/clash/bin/clash && \
-# chmod +x files/opt/clash/bin/clash
+releasemihomo=$(curl -s -L https://github.com/MetaCubeX/mihomo/releases/latest | grep "title>Release" | cut -d " " -f 4)
+curl -L https://github.com/MetaCubeX/mihomo/releases/download/$releasemihomo/mihomo-linux-arm64-$releasemihomo.gz -o /tmp/clash.gz
+mkdir -p files/opt/clash/bin && \
+gunzip -c /tmp/clash.gz > files/etc/openclash/core/clash_meta && \
+chmod +x files/etc/openclash/core/clash_meta
