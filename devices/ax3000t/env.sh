0fc75e9ef909305a2e936e6e54883d90858975b0
Mark Glants <mark@glants.xyz>
2026-01-08 05:45:33 +0300

    feat: add ax3000t

---
+++ devices/ax3000t/env.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/env bash
+# Copyright (c) 2022 The Parca Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -euo pipefail
+OPENWRT_CHANNEL="release"
+# renovate: datasource=git-tags depName=https://github.com/openwrt/openwrt.git
+OPENWRT_VERSION="v24.10.5"
+
+TARGET="mediatek"
+SUBTARGET="filogic"
+
+# Must match an ImageBuilder PROFILE exactly
+PROFILE="xiaomi_mi-router-ax3000t"
+# -----------------------------------------------------------------------------
+# opkg feed architecture
+# -----------------------------------------------------------------------------
+# This must match the directory name in your binary feed
+OPKG_ARCH="aarch64_cortex-a53"
+
+# -----------------------------------------------------------------------------
+# Optional metadata (not required by the workflow, but useful later)
+# -----------------------------------------------------------------------------
+DEVICE_NAME="ax3000t"
+DEVICE_VENDOR="xiaomi"
+DEVICE_MODEL="xiaomi-router-ax3000t"
